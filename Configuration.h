b00de54bff3db3760d3f5223a3d4a0101a13710b
po456asd <y_love_conan@hotmail.com>
2021-07-02 10:54:16 +0700

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -145,7 +145,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "white v1.1"
+#define CUSTOM_MACHINE_NAME "white v1.2 Push to git edition"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
