88e693dbf3ba102c82dd596046dc1fb7a8237f91
po456asd <y_love_conan@hotmail.com>
2021-07-02 11:25:25 +0700

    this one is a base patch for my anet a8 plus with skr mini e3 v1.2

--- Configuration.h
+++ Configuration.h
@@ -145,7 +145,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "white v1.51 i think this one work."
+#define CUSTOM_MACHINE_NAME "white v1.52 just changed name to say this one work."
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
