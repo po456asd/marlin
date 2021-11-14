ac14c4329d6cc9c98e780a447b17994b1c983806
po456asd <y_love_conan@hotmail.com>
2021-11-14 15:44:56 +0700

    Update Configuration.h

--- Skr Mini E3 v2/Marlin-2.0.x/Marlin/Configuration.h
+++ Skr Mini E3 v2/Marlin-2.0.x/Marlin/Configuration.h
@@ -489,7 +489,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  *
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -497,7 +497,7 @@
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_6 0
 #define TEMP_SENSOR_7 0
-#define TEMP_SENSOR_BED 1
+#define TEMP_SENSOR_BED 5
 #define TEMP_SENSOR_PROBE 0
 #define TEMP_SENSOR_CHAMBER 0
 #define TEMP_SENSOR_COOLER 0
